78923bb1a3339b7f226e0644c82061313494cb56
Ihor <tailormoon@icloud.com>
2023-11-04 11:26:41 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -112,12 +112,8 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     libdisplayconfig.qti \
     libion \
-    vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
     libdisplayconfig.qti
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
