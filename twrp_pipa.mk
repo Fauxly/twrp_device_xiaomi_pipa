1d23d39d5a972d5b2de20bd1205f9192c5c9cb3d
Ihor <tailormoon@icloud.com>
2023-11-04 22:14:14 +0200

    00

--- twrp_pipa.mk
+++ twrp_pipa.mk
@@ -5,29 +5,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := pipa
-
-# 引用默认的编译配置文件，core_64_bit必须先于其他引用，否则不生效
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pipa
 PRODUCT_NAME := twrp_pipa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 23043RP34G
-PRODUCT_MANUFACTURER := Xiaomi
-#PRODUCT_RELEASE_NAME := Xiaomi Pad 6
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="pipa_global-user 13 RKQ1.211001.001 V14.0.8.0.TMZMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V14.0.8.0.TMZMIXM:user/release-keys
