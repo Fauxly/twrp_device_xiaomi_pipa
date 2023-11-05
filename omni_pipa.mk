21c5684ffefe7cc1135c5dc24202bc95f80748bc
Ihor <tailormoon@icloud.com>
2023-11-05 14:23:26 +0200

    df

--- omni_pipa.mk
+++ omni_pipa.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
 PRODUCT_DEVICE := pipa
-PRODUCT_NAME := omni_pipa
+PRODUCT_NAME := twrp_pipa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 23043RP34G
 PRODUCT_MANUFACTURER := xiaomi
