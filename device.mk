7d7e00cfa2ad888b6d696d4c49ffe616bcc4482c
Ihor <tailormoon@icloud.com>
2023-11-05 14:17:38 +0200

    up

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.kona
+    bootctrl.kona.recovery
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.kona \
